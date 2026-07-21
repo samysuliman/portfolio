027ec336d97decec37cc06c95ca1bd4e23c29687
samysuliman <samysuliman494@gmail.com>
2026-07-21 12:46:48 +0300

    Delete articles/articles-data.js
    Update published articles

--- articles/articles-data.js
+++
@@ -1,3 +0,0 @@
-// Public article data. Generated from publisher.html.
-// Clean start: publish the first article from publisher.html, then upload the exported file here.
-window.SAMY_ARTICLES = [];
